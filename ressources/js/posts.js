cbbc9d715410db14e0d96593ef8aa7b944c9b2cd
Cédric Champetier <cedric.champetier@ubikasec.com>
2026-03-08 15:42:32 +0100

    feat: scroll de l'entete sur index + post

--- ressources/js/posts.js
+++ ressources/js/posts.js
@@ -1,26 +1,3 @@
-// Gestion du masquage du header lors du scroll
-let lastScrollTop = 0;
-const header = document.querySelector('.header');
-let scrollTimeout;
-
-window.addEventListener('scroll', () => {
-    clearTimeout(scrollTimeout);
-    
-    scrollTimeout = setTimeout(() => {
-        const scrollTop = window.scrollY || document.documentElement.scrollTop;
-        
-        if (scrollTop > lastScrollTop && scrollTop > 100) {
-            // Scroll vers le bas - cacher le header
-            header.classList.add('header-hidden');
-        } else if (scrollTop < lastScrollTop) {
-            // Scroll vers le haut - afficher le header
-            header.classList.remove('header-hidden');
-        }
-        
-        lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
-    }, 50);
-});
-
 document.addEventListener('alpine:init', () => {
     Alpine.data('postInteractions', (postId) => ({
         postId: postId,
